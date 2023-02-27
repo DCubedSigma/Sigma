e6922a36daab5c97ab06a1d0ffd7c8737b0ce71d
DCubedSigma <125845854+DCubedSigma@users.noreply.github.com>
2023-02-27 17:18:44 +0800

    Update Kugou.js

--- Kugou.js
+++ Kugou.js
@@ -8,51 +8,48 @@
 群1077223830
 *******************************
 [rewrite_local]
-^http[s]?:\/\/gateway.kugou.com.+$ url script-response-body https://raw.githubusercontent.com/DCubedSigma/Sigma/main/Kugou.js
+^http[s]?:\/\/gateway.kugou.com\/mobile\/vipinfoV2&code=6CD6CC814DA3F20219DB3D9AA005EA8B954078A151043C1A4F16528893F650E85630DD57E638700460D82EB30CFD18BDAF47EE94F279D52856BE188DCE7EF40B2E3D27AD7E29EB37A0F63E1D8588336033DE36C6B256214141C8795F08FA090202191FB5FFAEED6738BE85AD3BAF08BE7972E3E3DA31BC0A1BA388E43EE118C5DFF89C0EC25020FC435BB81040DEF4A12A6321406DD6EF56F54B16D58FBA2CF0ACD0E1D5F27AD8D777721ED440C383A278BC17F0FD3C64BBBC6D343241C75C60E8C50A088531D72E2963127BF74CEBEAC03A74E40DAE6E5C8A2099EE5C40D5C7A44A09CBBD8F291DE3AE6CEAB609FD17AE50AA60BE7F88FDCC5220411033AD0E4FD54AA22EC234EBBB73C1876E577C1E7D4413B88456066321A18BF09A85AF3A0CDB1329FACA5954D0AB8D7B309938785B4F50AE62ECD6D993604970BCB05F454D95324D00C3EF64F2BB1611BA12AFEBEB77C87F0A7F4B42EB1CCA156FAACBA195B728ACD9BB2D45.+$ url script-response-body kugou.js
 [mitm] 
 hostname = *.kugou.*
 *******************************
 Surge
 
 [Script]
-^http[s]?:\/\/gateway.kugou.com.+$ requires-body=1,max-size=0,script-path=kugou.js
+^http[s]?:\/\/gateway.kugou.com\/mobile\/vipinfoV2&code=6CD6CC814DA3F20219DB3D9AA005EA8B954078A151043C1A4F16528893F650E85630DD57E638700460D82EB30CFD18BDAF47EE94F279D52856BE188DCE7EF40B2E3D27AD7E29EB37A0F63E1D8588336033DE36C6B256214141C8795F08FA090202191FB5FFAEED6738BE85AD3BAF08BE7972E3E3DA31BC0A1BA388E43EE118C5DFF89C0EC25020FC435BB81040DEF4A12A6321406DD6EF56F54B16D58FBA2CF0ACD0E1D5F27AD8D777721ED440C383A278BC17F0FD3C64BBBC6D343241C75C60E8C50A088531D72E2963127BF74CEBEAC03A74E40DAE6E5C8A2099EE5C40D5C7A44A09CBBD8F291DE3AE6CEAB609FD17AE50AA60BE7F88FDCC5220411033AD0E4FD54AA22EC234EBBB73C1876E577C1E7D4413B88456066321A18BF09A85AF3A0CDB1329FACA5954D0AB8D7B309938785B4F50AE62ECD6D993604970BCB05F454D95324D00C3EF64F2BB1611BA12AFEBEB77C87F0A7F4B42EB1CCA156FAACBA195B728ACD9BB2D45.+$ requires-body=1,max-size=0,script-path=kugou.js
 
 [MITM]
 hostname = *.kugou.*
 
 *******************************/
 var obj = JSON.parse($response.body);
-    obj.data.user_type= 1;
-obj.data.user_y_type= 1;
-obj.data.is_vip= 1;
-obj.data.vip_type= 1;
-obj.data.vip_type= 1;
-obj.data.svip_score= 1;
-obj.data.svip_level= 1;
-obj.data.annual_fee_begin_time= 09";
-obj.data.annual_fee_end_time= 09";
-obj.data.su_vip_end_time= 09";
-obj.data.vip_end_time= 09";
-obj.data.roam_type= 1;
-obj.data.roam_end_time= 36";
-obj.data.m_y_endtime= 36";
-obj.data.vip_y_endtime= 36";
-obj.data.m_type= 1;
-obj.data.m_is_old= 1;
-obj.data.su_vip_y_endtime= 36";
-obj.data.roam_begin_time= 36";
-obj.data.y_type= 1;
-obj.data.signed_svip_before= 1;
-obj.data.end_time= 36";
-obj.data.duration= 873616277659;
-obj.data.p_grade= 20;
-obj.data.p_current_point= 873616277659;
-obj.data.p_next_grade= 20;
-obj.data.m_reset_time= 36",;
-obj.data.vip_clearday= 36";
-obj.data.h_signed= 36";
-obj.data.m_clearday= 36";
-obj.data.su_vip_clearday= 36";
-obj.data.vip_list= 36";
-obj.data.m_end_time= 09";
+    obj.user_type=  1;
+obj.user_y_type=  1;
+obj.is_vip=  1;
+obj.vip_type=  1;
+obj.svip_score=  1;
+obj.svip_level=  1;
+obj.annual_begin_time= 09";
+obj.annual_begin_time= 09";
+obj.su_vip_end_time= 09";
+obj.vip_end_time= 09";
+obj.roam_type=  1;
+obj.roam_end_time= 36";
+obj.m_y_endtime= 36";
+obj.vip_y_endtime= 36";
+obj.m_type=  1;
+obj.m_is_old=  1;
+obj.su_vip_y_endtime= 36";
+obj.roam_begin_time= 36";
+obj.y_type=  1;
+obj.signed_svip_before=  1;
+obj.end_time= 36";
+obj.duration=  873616277659;
+obj.p_grade=  20;
+obj.p_current_point=  873616277659;
+obj.p_next_grade=  20;
+obj.m_reset_time= 36";
+obj.vip_clearday= 36";
+obj.su_vip_clearday= 36";
+obj.vip_list= 36";
+obj.m_end_time= 09";
     $done({body: JSON.stringify(obj)});
